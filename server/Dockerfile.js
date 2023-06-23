4778c845b152d93030f4aea253385543feb24146
Sam Rosser <sam.rosser@yahoo.co.uk>
2023-06-23 12:45:33 +0100

    Update docker file

--- server/Dockerfile.js
+++
@@ -1,8 +0,0 @@
-# syntax=docker/dockerfile:1
-   
-FROM node:18-alpine
-WORKDIR /mern-docker-template/server
-COPY . /server/
-RUN yarn install --production
-# CMD ["node", "app/src/index.js"]
-EXPOSE 3000
